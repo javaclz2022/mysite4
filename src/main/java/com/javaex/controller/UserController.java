41beadf5c8f78eb5545141b682b841a46bc8bbe6
javaclz2022 <javaclz2022@gmail.com>
2022-06-20 15:18:06 +0900

    mysite4

--- src/main/java/com/javaex/controller/UserController.java
+++ src/main/java/com/javaex/controller/UserController.java
@@ -12,29 +12,34 @@ import com.javaex.vo.UserVo;
 @Controller
 public class UserController {
 
-	//필드
+	// 필드
 	@Autowired
 	UserService userService;
-	
-	
-	//회원가입
-	@RequestMapping(value="/user/join", method = {RequestMethod.GET, RequestMethod.POST})
+
+	// 로그인
+	@RequestMapping(value = "/user/loginForm", method = { RequestMethod.GET, RequestMethod.POST })
+	public String loginForm() {
+		System.out.println("UserController>loginForm()");
+
+		return "user/loginForm";
+	}
+
+	// 회원가입
+	@RequestMapping(value = "/user/join", method = { RequestMethod.GET, RequestMethod.POST })
 	public String join(@ModelAttribute UserVo userVo) {
 		System.out.println("UserController>join()");
-		
-		
-		userService.join(userVo);
-		
-		return "";
+
+		int count = userService.join(userVo);
+		System.out.println("UserController: " + count);
+
+		return "user/joinOk";
 	}
-	
-	
-	
-	//회원가입폼
-	@RequestMapping(value="/user/joinForm", method = {RequestMethod.GET, RequestMethod.POST})
+
+	// 회원가입폼
+	@RequestMapping(value = "/user/joinForm", method = { RequestMethod.GET, RequestMethod.POST })
 	public String joinForm() {
 		System.out.println("UserController>joinForm()");
-		
+
 		return "user/joinForm";
 	}
 }
