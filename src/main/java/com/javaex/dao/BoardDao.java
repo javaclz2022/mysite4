8628754b820a5966fe63650232230a7e7ce53526
javaclz2022 <javaclz2022@gmail.com>
2022-07-05 11:23:51 +0900

    board

--- src/main/java/com/javaex/dao/BoardDao.java
+++ src/main/java/com/javaex/dao/BoardDao.java
@@ -14,38 +14,77 @@ public class BoardDao {
 	@Autowired
 	private SqlSession sqlSession;
 
-	
-	public List<BoardVo> selectList3(String keyword) {
-		System.out.println("BoardDao>selectList3()");
+	//리스트(일반)
+	public List<BoardVo> selectList4(){
+		System.out.println("BoardDao>selectList4()");
 		
-		List<BoardVo> boardList = sqlSession.selectList("board.selectList3", keyword);
+		List<BoardVo> boardList = sqlSession.selectList("board.selectList4");
 		
 		return boardList;
 	}
 	
 	
 	
+	public List<BoardVo> selectList3(String keyword) {
+		System.out.println("BoardDao>selectList3()");
+
+		List<BoardVo> boardList = sqlSession.selectList("board.selectList3", keyword);
+
+		return boardList;
+	}
+
 	// 글전체 가져오기(리스트만 출력할때)
 	public List<BoardVo> selectList() {
 		System.out.println("BoardDao>selectList()");
 
 		List<BoardVo> boardList = sqlSession.selectList("board.selectList");
-		
+
 		return boardList;
 	}
 
-	
-	public List<BoardVo> selectList2(String keyword){
+	public List<BoardVo> selectList2(String keyword) {
 		System.out.println("BoardDao>selectList2()");
 		System.out.println(keyword);
-		
+
 		List<BoardVo> boardList = sqlSession.selectList("board.selectList2", keyword);
 		return boardList;
 	}
+
+	// 글저장
+	public int insert(BoardVo boardVo) {
+		System.out.println("boardDao>insert");
+
+		return sqlSession.insert("board.insert", boardVo);
+	}
+
+	// 글 1개 가져오기
+	public BoardVo select(int no) {
+		System.out.println("boardDao>select");
+
+		return sqlSession.selectOne("board.selcet", no);
+	}
+
+	// 조회수 업데이트
+	public int updateHit(int no) {
+		System.out.println("boardDao>updateHit");
+
+		return sqlSession.update("board.updateHit", no);
+	}
+
+	// 글수정
+	public int update(BoardVo boardVo) {
+		System.out.println("boardDao>update");
+
+		return sqlSession.update("board.update", boardVo);
+	}
+
+	// 글삭제
+	public int delete(BoardVo boardVo) {
+		System.out.println("boardDao/delete");
+
+		return sqlSession.delete("board.delete", boardVo);
+	}
 	
 	
-	
-	
-	
-	
+
 }
--- src/main/java/com/javaex/service/BoardService.java
+++ src/main/java/com/javaex/service/BoardService.java
@@ -14,35 +14,80 @@ public class BoardService {
 	@Autowired
 	private BoardDao boardDao;
 
-	
-	public List<BoardVo> getBoardList3(String keyword) {
-		System.out.println("BoardService>getBoardList3()");
-
-		List<BoardVo> boardList = boardDao.selectList3(keyword);	
+	//리스트(일반)
+	public List<BoardVo> getBoardList4(){
+		System.out.println("BoardService>getBoardList4()");
+		
+		List<BoardVo> boardList = boardDao.selectList4();
 		
 		return boardList;
 	}
 	
 	
 	
+	
+	// 리스트(일반 + 검색)
+	public List<BoardVo> getBoardList3(String keyword) {
+		System.out.println("BoardService>getBoardList3()");
+
+		List<BoardVo> boardList = boardDao.selectList3(keyword);
+
+		return boardList;
+	}
+
 	// 리스트(리스트만 출력할때)
 	public List<BoardVo> getBoardList() {
 		System.out.println("BoardService>getBoardList()");
 
-		List<BoardVo> boardList = boardDao.selectList();	
-		
+		List<BoardVo> boardList = boardDao.selectList();
+
 		return boardList;
 	}
 
-	
-	public List<BoardVo> getBoardList2(String keyword){
+	// 리스트(검색했을때)
+	public List<BoardVo> getBoardList2(String keyword) {
 		System.out.println("BoardService>getBoardList2()");
-		
+
 		List<BoardVo> boardList = boardDao.selectList2(keyword);
-		
+
 		return boardList;
 	}
-	
-	
-	
+
+	// 글쓰기
+	public int addBoard(BoardVo boardVo) {
+		System.out.println("boardService>addBoard");
+
+		return boardDao.insert(boardVo);
+	}
+
+	// 글가져오기
+	public BoardVo getBoard(int no, String type) {
+		System.out.println("boardService>getBoard");
+
+		if ("read".equals(type)) {// 읽기 일때는 조회수 올림
+			boardDao.updateHit(no);
+			BoardVo boardVo = boardDao.select(no);
+			return boardVo;
+
+		} else { // 수정 일때는 조회수 올리지 않음
+			BoardVo boardVo = boardDao.select(no);
+			return boardVo;
+		}
+
+	}
+
+	// 글수정
+	public int modifyBoard(BoardVo boardVo) {
+		System.out.println("boardService>modifyBoard");
+
+		return boardDao.update(boardVo);
+	}
+
+	// 글삭제
+	public int removeBoard(BoardVo boardVo) {
+		System.out.println("boardService/removeBoard");
+
+		return boardDao.delete(boardVo);
+
+	}
 }
